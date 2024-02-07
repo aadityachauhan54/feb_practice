288b65cea30e0fe0c087a72d5037634eb45d9eeb
aaditya <aadityachauhan54@gmail.com>
2024-02-07 08:53:08 +0530

    string program

---
+++ src/gitexample2/GitJavaProgram.java
@@ -0,0 +1,16 @@
+package gitexample2;
+
+public class GitJavaProgram {
+
+	public static void main(String[] args) {
+		String str = "i am automation tester";
+		String ss[] = str.split(" ");
+		for(int i=ss.length-1;i>=0;i--) {
+			System.out.print(ss[i]+" ");
+		}
+
+		}
+
+	}
+
+
